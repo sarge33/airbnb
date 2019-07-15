9832f9cf4e01422f8388c64b0a213d1f5b606f3e
sarge33 <47280085+sarge33@users.noreply.github.com>
2019-07-15 08:52:01 -0700

    Update ListListIterator.java

--- ListListIterator.java
+++ ListListIterator.java
@@ -1,3 +1,51 @@
+import java.util.*;
+import org.junit.Test;
+import java.util.List;
+import static org.junit.Assert.*;
+
+class ListofListIterator implements Iterator<Integer> {
+    Iterator<List<Integer>> listIterator;
+    Iterator<Integer> itemIterator;
+
+    public ListofListIterator(List<List<Integer>> list) {
+        listIterator =  list.listIterator();
+        itemIterator = Collections.emptyIterator();
+    }
+    @Override
+    public boolean hasNext() {
+        while((itemIterator == null || !itemIterator.hasNext()) && listIterator.hasNext()) {
+            itemIterator = listIterator.next().iterator();
+        }
+        return itemIterator != null && itemIterator.hasNext();
+    }
+
+    @Override
+    public Integer next() {
+        return itemIterator.next();
+    }
+    
+    /*
+    Note: TO call remove() methode, next() method need to be called first:
+       to remove item by iterator, the iterator need to call next to move the iterator first then start remove
+       List<String> listOfBooks = new ArrayList<>();  
+       listOfBooks.add("Programming Pearls");
+       listOfBooks.add("Clean Code");
+       listOfBooks.add("Effective Java");
+       listOfBooks.add("Code Complete");
+        
+       Iterator<String> iterator = listOfBooks.iterator();
+       while(iterator.hasNext()){
+           String book = iterator.next();
+           listOfBooks.remove(book);
+       }
+    
+    */
+    @Override
+    public void remove() {
+        if (itemIterator != null)
+    }
+}
+
 class ListListIterator implements Iterator<Integer> {
     private Iterator<List<Integer>> rowIter;
     private Iterator<Integer> colIter;
@@ -25,5 +73,42 @@ class ListListIterator implements Iterator<Integer> {
             colIter = rowIter.next().iterator();
         if (colIter != null)
             colIter.remove();
+    }
+}
+
+public class MyUnitTest {
+   @Test
+    public void test() {
+        List<List<Integer>> test = new ArrayList<>();
+        test.add(new ArrayList<Integer>() {{
+            add(1);
+            add(2);
+        }});
+        test.add(new ArrayList<Integer>() {{
+            add(3);
+        }});
+        test.add(new ArrayList<Integer>() {{
+            add(4);
+            add(5);
+            add(6);
+        }});
+
+        System.out.println(" +++ 0 +++ ");
+        ListofListIterator sol = new ListofListIterator(test);
+        while (sol.hasNext()) {
+            System.out.println(sol.next() + " ");
+        }
+        sol = new ListofListIterator(test);
+        System.out.println(" +++ 1 +++ ");
+        while (sol.hasNext()) {
+            System.out.println("  -1- " + sol.next());
+            sol.remove();
+        }
+
+        System.out.println(" +++ 2 +++ ");
+        sol = new ListofListIterator(test);
+        while (sol.hasNext()) {
+            System.out.println(sol.next() + "  -2- ");
+        }
     }
 }
