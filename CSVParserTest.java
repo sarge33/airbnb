49d49ef5eae45f432042df237080f6a0874b7700
sarge33 <47280085+sarge33@users.noreply.github.com>
2019-09-02 12:05:31 -0700

    Update CSVParserTest.java

--- CSVParserTest.java
+++ CSVParserTest.java
@@ -1,57 +1,83 @@
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
 class CSVParser {
-    public String parse(String str) {
+
+    public String parseCSV(String str) {
         List<String> res = new ArrayList<>();
         boolean inQuote = false;
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i<str.length(); i++) {
+        for(int i = 0; i<str.length(); ++i) {
+            char ch = str.charAt(i);
             if(inQuote) {
-                if(str.charAt(i) == '\"') {
-                    if(i + 1 < str.length() && str.charAt(i+1) == '\"') {
-                        sb.append('\"'); // skip
-                        i++;
-                    } else {
-                        inQuote = false;
-                    }
+                if(i+1<str.length() && ch == '\"' && str.charAt(i+1) == '\"') {
+                    sb.append(ch);
+                    i++; // skip the 2nd "
+                } else if(ch == '\"'){
+                    inQuote = false;
                 } else {
-                    sb.append(str.charAt(i));
+                    sb.append(ch);
                 }
+
             } else {
-                if(str.charAt(i) == '\"') {
+                if(ch == '\"') {
                     inQuote = true;
-                } else if (str.charAt(i) == ',') {
+                } else if(ch == ',') {
                     res.add(sb.toString());
                     sb.setLength(0);
                 } else {
-                    sb.append(str.charAt(i));
+                    sb.append(ch);
                 }
             }
         }
-        if(sb.length() > 0) {
-            res.add(sb.toString());
-        }
-
+        if(sb.length() > 0) res.add(sb.toString());
         return String.join("|", res);
     }
+
 }
-public class CSVParserTest {
 
-    public static void main(String[] args) {
-
-        String original = "John,Smith,john.smith@gmail.com,Los Angeles,1";
+public class Test7 {
+    @Test
+    public void test1() {
+        CSVParser sol = new CSVParser();
+        String test = "John,Smith,john.smith@gmail.com,Los Angeles,1";
         String expected = "John|Smith|john.smith@gmail.com|Los Angeles|1";
+        assertEquals(expected, sol.parseCSV(test));
 
-        CSVParser parser = new CSVParser();
-        String s = parser.parse(original);
-        System.out.println("original: " + original);
-        System.out.println("expected: " + expected);
-        System.out.println("test    : " + s);
+        test = "Jane,Roberts,janer@msn.com,\"San Francisco, CA\",0";
+        expected = "Jane|Roberts|janer@msn.com|San Francisco, CA|0";
+        assertEquals(expected, sol.parseCSV(test));
 
-        original = "\"Alexandra \"\"Alex\"\"\",Menendez,alex.menendez@gmail.com,Miami,1";
+        test = "\"Alexandra \"\"Alex\"\"\",Menendez,alex.menendez@gmail.com,Miami,1";
         expected = "Alexandra \"Alex\"|Menendez|alex.menendez@gmail.com|Miami|1";
+        assertEquals(expected, sol.parseCSV(test));
 
-        s = parser.parse(original);
-        System.out.println("original: " + original);
-        System.out.println("expected: " + expected);
-        System.out.println("test    : " + s);
-   }
- }
+        test = "\"\"\"Alexandra Alex\"\"\"";
+        expected = "\"Alexandra Alex\"";
+        assertEquals(expected, sol.parseCSV(test));
+    }
+
+    @Test
+    public void test2() {
+        CSVParser sol = new CSVParser();
+        String test = "John,Smith,john.smith@gmail.com,Los Angeles,1";
+        String expected = "John|Smith|john.smith@gmail.com|Los Angeles|1";
+        assertEquals(expected, sol.parseCSV(test));
+
+        test = "Jane,Roberts,janer@msn.com,\"San Francisco, CA\",0";
+        expected = "Jane|Roberts|janer@msn.com|San Francisco, CA|0";
+        assertEquals(expected, sol.parseCSV(test));
+
+        test = "\"Alexandra \"\"Alex\"\"\",Menendez,alex.menendez@gmail.com,Miami,1";
+        expected = "Alexandra \"Alex\"|Menendez|alex.menendez@gmail.com|Miami|1";
+        assertEquals(expected, sol.parseCSV(test));
+
+        test = "\"\"\"Alexandra Alex\"\"\"";
+        expected = "\"Alexandra Alex\"";
+        assertEquals(expected, sol.parseCSV(test));
+    }
+
+}
